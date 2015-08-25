6c3edc6f3a610f2e8da6c94bfd3233862321dda9
eranshac <eranmatematika@gmail.com>
2015-08-26 02:45:58 +0300

    sound

---
+++ UniversityProject/Assets/Scripts/PopSoundFX.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopSoundFX : MonoBehaviour {
+
+	public AudioSource myAudioSource;
+	void Start () {
+		myAudioSource.Play ();
+		Invoke ("DestroySoundFx", myAudioSource.clip.length);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void DestroySoundFx(){
+		Destroy (gameObject);
+	}
+}
