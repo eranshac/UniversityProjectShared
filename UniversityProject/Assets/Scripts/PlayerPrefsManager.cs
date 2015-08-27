001d287412db3c727c896e7ecedbd7c38f59694e
omrisap <omrisap@walla.co.il>
2015-08-28 02:22:32 +0300

    GetUserPitch

---
+++ UniversityProject/Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsManager : MonoBehaviour {
+		
+	const string Highest_Pitch_Key = "Highest Pitch";	
+	const string Lowest_Pitch_Key = "Lowest Pitch";	
+	
+	
+	// Use this for initialization
+	
+	public static void SetHighestPitch(float pitch){
+	
+		PlayerPrefs.SetFloat(Highest_Pitch_Key,pitch);
+	}
+	
+	private static float GetHighestPitch(){
+	
+		return PlayerPrefs.GetFloat(Highest_Pitch_Key);
+	
+	}
+	
+	public static void SetLowhestPitch(float pitch){
+		
+		PlayerPrefs.SetFloat(Lowest_Pitch_Key,pitch);
+	}
+	
+	private static float GetLowhestPitch(){
+		
+		return PlayerPrefs.GetFloat(Lowest_Pitch_Key);
+		
+	}
+	
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
